31ae904ae8fb9d4fa7b3c37c75e3511bdc4ce824
Ardis Lu <ardisglu@gmail.com>
2023-04-15 20:35:30 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ERC-2098
+
+An informational website and minimal demonstration of [ERC-2098](https://eips.ethereum.org/EIPS/eip-2098).
---
+++ erc-2098.js
@@ -0,0 +1,13 @@
+/**
+ * Convert a canonical Ethereum signature into an ERC-2098 compact signature.
+ * @param {string} signature - The canonical signature to convert.
+ * @returns {string} The ERC-2098 compact signature.
+ */
+export function toCompact(signature) {
+  if (signature[130] === 'b') {
+    return signature.substring(0, 128);
+  }
+  // 578...968n === 1n << 255n
+  const compactS = BigInt(`0x${signature.substring(66, 128)}`) | 57896044618658097711785492504343953926634992332820282019728792003956564819968n;
+  return `${signature.substring(0, 66)}${compactS.toString(16)}`;
+}
